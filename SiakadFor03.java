5a6fe873eefea1f9be0d32d57f7a19b7db72621f
Vallent12 <fevadtya09.gmail.com>
2025-10-25 16:07:10 +0700

    Modifikasi

--- SiakadFor03.java
+++ SiakadFor03.java
@@ -5,6 +5,7 @@ public class SiakadFor03 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++ ) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,10 +16,18 @@ public class SiakadFor03 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60 && nilai <= 100) {
+                lulus++;
+            }
+            if (nilai < 60 && nilai > 0) {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi " + tertinggi);
         System.out.println("Nilai terendah " + terendah);
+        System.out.println("Total mahasiswa yang lulus: " + lulus);
+        System.out.println("Total mahasiswa yang tidak lulus " + tidakLulus);
 
     }
 
